1994b14ea471dace75f37c5524ddbb1a77021c90
Masri Alemu <masrialemu@gmail.com>
2023-10-10 14:00:02 +0300

    v1.4

--- Blogging Platform/script/signup.js
+++ Blogging Platform/script/signup.js
@@ -1,29 +1,31 @@
-const usernameInput = document.getElementById("username");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const confirmPasswordInput = document.getElementById("confirm-password");
-const acceptTermsCheckbox = document.getElementById("accept-terms");
-const signUpButton = document.querySelector(".signup-button");
+// Get references to the form and input fields
+const signUpForm = document.getElementById('signup-form');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirm-password');
+const signUpButton = document.querySelector('.signup-button');
 
-// Add event listeners to input fields and checkbox for validation
-usernameInput.addEventListener("input", validateForm);
-emailInput.addEventListener("input", validateForm);
-passwordInput.addEventListener("input", validateForm);
-confirmPasswordInput.addEventListener("input", validateForm);
-acceptTermsCheckbox.addEventListener("change", validateForm);
-
-// Function to check if the form is valid
+// Function to enable/disable the "Sign Up" button based on input validation
 function validateForm() {
-    const isUsernameValid = usernameInput.value.trim() !== "";
-    const isEmailValid = emailInput.checkValidity();
-    const isPasswordValid = passwordInput.value.length >= 6;
-    const doPasswordsMatch = confirmPasswordInput.value === passwordInput.value;
-    const isTermsAccepted = acceptTermsCheckbox.checked;
+    const username = usernameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
 
-    signUpButton.disabled = !(isUsernameValid && isEmailValid && isPasswordValid && doPasswordsMatch && isTermsAccepted);
+    // Check if all fields are filled and passwords match
+    if (username !== '' && email !== '' && password !== '' && confirmPassword === password) {
+        signUpButton.removeAttribute('disabled');
+    } else {
+        signUpButton.setAttribute('disabled', true);
+    }
 }
 
-// Prevent form submission (for testing purposes)
-document.getElementById("signup-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-});
+// Add event listeners to input fields for real-time validation
+usernameInput.addEventListener('input', validateForm);
+emailInput.addEventListener('input', validateForm);
+passwordInput.addEventListener('input', validateForm);
+confirmPasswordInput.addEventListener('input', validateForm);
+
+// Call the validateForm function to initially disable the button
+validateForm();
